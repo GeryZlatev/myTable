f7ccd5b39b0fc0aaff7e33634b0ab8abd4e1f6fd
GeryZlatev <68344068+GeryZlatev@users.noreply.github.com>
2021-01-05 13:43:26 +0200

    add tr and cells

---
+++ .history/app_20210105133512.js
@@ -0,0 +1,20 @@
+import { MOCK } from './MOCK_DATA.js';
+
+(function(data, document) {
+    let keys = Object.keys(data[0]);
+    let result = "";
+
+    result += '<table>';
+    result += '  <thead>';
+    result += '     <tr>';
+    result += keys.map(key => `<th>${key}</th>`).join(' ');
+    result += '     </tr>';
+    result += '  </thead>';
+    result += '</table>';
+
+
+
+
+    document.getElementById('app').innerHTML = result;
+
+}(MOCK, document))
---
+++ .history/app_20210105133708.js
@@ -0,0 +1,23 @@
+import { MOCK } from './MOCK_DATA.js';
+
+(function(data, document) {
+    let keys = Object.keys(data[0]);
+    let result = "";
+
+    result += '<table>';
+    result += '  <thead>';
+    result += '     <tr>';
+    result += keys.map(key => `<th>${key}</th>`).join(' ');
+    result += '     </tr>';
+    result += '  </thead>';
+    result += '   <tbody>'
+    result += data.map(row => keys.map)
+    result += '   </tbody>'
+    result += '</table>';
+
+
+
+
+    document.getElementById('app').innerHTML = result;
+
+}(MOCK, document))
